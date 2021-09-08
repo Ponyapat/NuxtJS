d8d190f8618bea95ba62e49b9c7ec60191b5f164
ponyapat <poonyapat_anusonthi@cmu.ac.th>
2021-09-09 01:03:46 +0700

    nuxtroute

---
+++ NuxtJS/Demo1/pages/admin.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>page external</h1>
+        <h2>Form user</h2>
+        <form>
+            <input type="text">
+            <input type="submit" value="insert">
+        </form>
+        <nuxt-child />
+        
+    </div>
+</template>
--- NuxtJS/Demo1/pages/users/index.vue
+++ NuxtJS/Demo1/pages/users/index.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1>Hello user page</h1>
+
+    <div>
+        <h1>user page internal</h1>
+        <h1>Display User Table</h1>
+        <table>
+            <tr>
+                <th>Name</th>
+            </tr>
+            <tr>
+                <th>A</th>
+            </tr>
+            <tr>
+                <th>B</th>
+            </tr>
+            <tr>
+                <th>C</th>
+            </tr>
+        </table>
+    </div>
 </template>
